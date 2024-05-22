3d0d5af2dc055da9f92fbffcad800ec8e02a0bf8
Tony Wu <tonywu93@protonmail.com>
2024-05-22 19:03:53 +1000

    implement b_sqr

--- fpga/common/include/binary_tower.h
+++ fpga/common/include/binary_tower.h
@@ -39,8 +39,21 @@ namespace binary_tower {
             ap_uint<W / 2> z0 = b_mul(a0, b0);
             ap_uint<W / 2> z1 = b_mul(a0 ^ a1, b0 ^ b1);
             ap_uint<W / 2> z2 = b_mul(a1, b1);
-
             return ((z1 ^ z0 ^ z2) ^ b_mul_alpha(z2), z0 ^ z2);
+        }
+    }
+
+    template<int W>
+    ap_uint<W> b_sqr(ap_uint<W> a) {
+#pragma HLS INLINE
+        if constexpr (W == 1) {
+            return a;
+        } else {
+            ap_uint<W / 2> a0 = a;
+            ap_uint<W / 2> a1 = a >> W / 2;
+            ap_uint<W / 2> a0_sqr = b_sqr(a0);
+            ap_uint<W / 2> a1_sqr = b_sqr(a1);
+            return (b_mul_alpha(a1_sqr), a0_sqr ^ a1_sqr);
         }
     }
 
@@ -53,7 +66,7 @@ namespace binary_tower {
             ap_uint<W / 2> a0 = a;
             ap_uint<W / 2> a1 = a >> W / 2;
             ap_uint<W / 2> a0z1 = a0 ^ b_mul_alpha(a1);
-            ap_uint<W / 2> delta = b_mul(a0, a0z1) ^ b_mul(a1, a1);
+            ap_uint<W / 2> delta = b_mul(a0, a0z1) ^ b_sqr(a1);
             ap_uint<W / 2> delta_inv = b_inv(delta);
             return (b_mul(delta_inv, a1), b_mul(delta_inv, a0z1));
         }
--- fpga/common/test/test_binary_tower.cpp
+++ fpga/common/test/test_binary_tower.cpp
@@ -13,11 +13,17 @@ ap_uint<32> dummy_b_inv(ap_uint<32> a) {
     return binary_tower::b_inv(a);
 }
 
+ap_uint<32> dummy_b_sqr(ap_uint<32> a) {
+#pragma HLS INTERFACE mode=ap_ctrl_none port=return
+#pragma HLS PIPELINE II=1
+    return binary_tower::b_sqr(a);
+}
+
 int main() {
-    ap_uint<16> a = 221;
-    ap_uint<16> b = 168;
-    ap_uint<16> c = binary_tower::b_mul(a, b);
-    ap_uint<16> c_inv = binary_tower::b_inv(c);
+    ap_uint<32> a = 221;
+    ap_uint<32> b = 168;
+    ap_uint<32> c = binary_tower::b_mul(a, b);
+    ap_uint<32> c_inv = binary_tower::b_inv(c);
     std::cout << c_inv << std::endl;
     std::cout << binary_tower::b_mul(c, c_inv) << std::endl;
     return 0;
