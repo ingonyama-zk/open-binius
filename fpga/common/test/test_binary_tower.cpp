8309cdb43d97ad5ad9125b06002073946ae7b0e9
Tony Wu <tonywu93@protonmail.com>
2024-05-22 16:44:50 +1000

    remove lookups for inversion

--- fpga/common/test/test_binary_tower.cpp
+++ fpga/common/test/test_binary_tower.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 #include "binary_tower.h"
 
-ap_uint<32> dummy(ap_uint<32> a, ap_uint<32> b) {
+ap_uint<32> dummy_b_mul(ap_uint<32> a, ap_uint<32> b) {
 #pragma HLS INTERFACE mode=ap_ctrl_none port=return
 #pragma HLS PIPELINE II=1
     return binary_tower::b_mul(a, b);
 }
 
+ap_uint<32> dummy_b_inv(ap_uint<32> a) {
+#pragma HLS INTERFACE mode=ap_ctrl_none port=return
+#pragma HLS PIPELINE II=1
+    return binary_tower::b_inv(a);
+}
+
 int main() {
-    ap_uint<8> a = 221;
-    ap_uint<8> b = 168;
-    std::cout << binary_tower::b_mul(a, b) << std::endl;
+    ap_uint<16> a = 221;
+    ap_uint<16> b = 168;
+    ap_uint<16> c = binary_tower::b_mul(a, b);
+    ap_uint<16> c_inv = binary_tower::b_inv(c);
+    std::cout << c_inv << std::endl;
+    std::cout << binary_tower::b_mul(c, c_inv) << std::endl;
     return 0;
 }
