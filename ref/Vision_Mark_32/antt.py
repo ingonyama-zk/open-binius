8d471ca5d2e7a51884f91cb07d5ec09d2f4e1d3e
yuvalingo <106463468+yuvalingo@users.noreply.github.com>
2024-08-08 17:11:16 +0300

    add additive ntt

---
+++ ref/Vision_Mark_32/antt.py
@@ -0,0 +1,169 @@
+# from btf_cantor import *
+from btf_binius import *
+
+
+class antt():
+    def __init__(self, N):
+        self.N = N
+        self.logN = math.log2(N)
+        assert self.logN == int(self.logN)
+        self.logN = int(self.logN)
+        self.loglogN = math.ceil(math.log2(self.logN))  # nlvls
+        self._init_spolys()
+
+    def _init_spolys(self):
+        # s_poly[:, 0] = coefficients, s_poly[:, 1] = exponents
+        # init at x
+        s_poly = np.atleast_2d(np.array([btf(1, nlvls=self.loglogN), 1]).astype(object))
+        s_polys = [s_poly]
+        for i in range(1, self.logN):
+            # square previous
+            tmp = self.poly_mult(s_poly, s_poly)
+            # scalar mult previous
+            scl = self.s_poly_eval(s_poly, btf(1 << (i - 1)))
+            for j in range(s_poly.shape[0]):
+                s_poly[j, 0] *= scl
+            # add square and scalar mult
+            s_poly = self.poly_add(s_poly, tmp)
+            s_polys += [s_poly]
+        self.s_polys = s_polys
+
+    @staticmethod
+    def poly_mult(a, b):
+        c = np.atleast_2d(np.array([btf(0), 0]).astype(object))
+        for i in range(a.shape[0]):
+            for j in range(b.shape[0]):
+                tmp = np.array([a[i, 0] * b[j, 0], a[i, 1] + b[j, 1]])
+                idx = np.where(c[:, 1] == tmp[1])[0]
+                if len(idx) == 0:
+                    c = np.vstack((c, tmp))
+                elif len(idx) == 1:
+                    c[idx[0], 0] += tmp[0]
+                else:
+                    assert False
+        c = c[1:, :]
+        return c
+
+    @staticmethod
+    def poly_add(a, b):
+        c = copy.copy(a)
+        for j in range(b.shape[0]):
+            tmp = b[j, :]
+            idx = np.where(c[:, 1] == tmp[1])[0]
+            if len(idx) == 0:
+                c = np.vstack((c, tmp))
+            elif len(idx) == 1:
+                c[idx[0], 0] += tmp[0]
+            else:
+                assert False
+        tmp = c[:, 0]
+        return c[c[:, 0] != 0, :]
+
+    def s_poly_eval(self, i, x):
+        try:
+            s_poly = self.s_polys[i]
+        except (AttributeError, TypeError):
+            s_poly = i
+        x.extend_nlvls(self.loglogN)  # extend to prevent aliasing (modulo)
+        tmp = btf(0)
+        for s, e in s_poly:
+            tmp += s * x ** e
+        return tmp
+
+    def fft(self, x, coset=btf(0)):
+        # init
+        N = len(x)
+        assert N <= self.N
+        logN = math.log2(N)
+        assert logN == int(logN)
+        logN = int(logN)
+        if logN == 0:
+            return x
+        # split
+        x_0 = x[:N // 2]
+        x_1 = x[N // 2:]
+        # twiddle
+        s = self.s_poly_eval(logN - 1, coset)
+        s *= self.s_poly_eval(logN - 1, btf(1 << (logN - 1))).inv()
+        # butterflies
+        q_0 = x_0 + s * x_1
+        q_1 = q_0 + x_1
+        # recurse
+        X_0 = self.fft(q_0, coset)
+        X_1 = self.fft(q_1, coset + btf(1 << (logN - 1)))
+        return np.concatenate((X_0, X_1))
+
+    def ifft(self, X, coset=btf(0)):
+        # init
+        N = len(X)
+        assert N <= self.N
+        logN = math.log2(N)
+        assert logN == int(logN)
+        logN = int(logN)
+        if logN == 0:
+            return X
+        # split
+        X_0 = X[:N // 2]
+        X_1 = X[N // 2:]
+        # recurse
+        Q_0 = self.ifft(X_0, coset)
+        Q_1 = self.ifft(X_1, coset + btf(1 << (logN - 1)))
+        # twiddle
+        s = self.s_poly_eval(logN - 1, coset)
+        s *= self.s_poly_eval(logN - 1, btf(1 << (logN - 1))).inv()
+        # butterflies
+        x_1 = Q_0 + Q_1
+        x_0 = Q_0 + s * x_1
+        return np.concatenate((x_0, x_1))
+
+
+def antt_matrix(logN = 3):
+    N = 2 ** logN
+    loglogN = int(math.ceil(math.log2(logN)))
+    afft_ = antt(N)
+    # x_poly[:, 0] = coefficients, x_poly[:, 1] = exponents
+    # init at 1
+    const = np.atleast_2d(np.array([btf(1, nlvls=loglogN), 0]).astype(object))
+    x_polys = [const]
+    for i in range(1, 2 ** logN):
+        x_poly = const
+        i_bin = bin(i)[2:].zfill(logN)[::-1]
+        for j in range(logN):
+            j_bin = i_bin[j]
+            if j_bin == '1':
+                x_poly = afft_.poly_mult(x_poly, afft_.s_polys[j])
+        x_polys += [x_poly]
+    #
+    F = np.full((N, N), btf(0)).astype(object)
+    for i in range(N):
+        for j in range(N):
+            F[i, j] = afft_.s_poly_eval(x_polys[j], btf(i, nlvls=loglogN))
+    print(F)
+    print()
+    a = np.array([btf(np.random.randint(N)) for i in range(N)]).astype(object)
+    print(F @ a)
+    print(afft_.fft(a))
+    print()
+    print(np.matmul(F, F.T))
+
+
+if __name__ == '__main__':
+    # test matrix form
+    antt_matrix()
+
+    # random test
+    logN = 8
+    antt_ = antt(2 ** logN)
+    a = np.array([btf(np.random.randint(2 ** logN)) for i in range(2 ** logN)]).astype(object)
+    print(a)
+    b = antt_.fft(a)
+    print(b)
+    c = antt_.ifft(b)
+    print(c)
+    assert np.array_equal(a, c)
+
+    # print beta series
+    logN = 16
+    antt_ = antt(2 ** logN)
+    for i in range(logN):
+        print(f'{i},{antt_.s_poly_eval(i, btf(2 ** i))}')
---
+++ ref/Vision_Mark_32/btf_cantor.py
@@ -0,0 +1,161 @@
+import math
+import copy
+import numpy as np
+
+
+class btf():
+    def __init__(self, val, nlvls=None):
+        if nlvls is None:
+            try:
+                nlvls = int(math.log2(math.log2(val))) + 1
+            except ValueError:
+                nlvls = 1
+        self._set_nlvls(nlvls)
+        self._val = int(val) % self._p
+        assert self._val >= 0
+
+    def _set_nlvls(self, nlvls):
+        assert nlvls > 0
+        self.nlvls = nlvls
+        self.nbits = 2 ** self.nlvls
+        self.nhalfbits = 2 ** (self.nlvls - 1)
+        self._p = 2 ** self.nbits
+
+    def __str__(self):
+        return f'{self._val}'
+
+    __repr__ = __str__
+
+    def zero(self):
+        self._val = 0
+
+    def one(self):
+        self._val = 1
+
+    def extend_nlvls(self, nlvls):
+        assert self.nlvls <= nlvls
+        self._set_nlvls(nlvls)
+
+    def val(self):
+        return self._val
+
+    def bin(self):
+        return bin(self._val)[2:].zfill(self.nbits)
+
+    def split(self):
+        hi = btf(int(self.bin()[:self.nhalfbits], 2), nlvls=self.nlvls - 1)
+        lo = btf(int(self.bin()[self.nhalfbits:], 2), nlvls=self.nlvls - 1)
+        return hi, lo
+
+    def __eq__(self, other):
+        if isinstance(other, int):
+            return self._val == other
+        elif isinstance(other, btf):
+            return self._val == other.val()
+        else:
+            return False
+
+    def __add__(self, other):
+        # align nlvls to max
+        this = copy.copy(self)
+        if this.nlvls > other.nlvls:
+            other.extend_nlvls(this.nlvls)
+        if other.nlvls > this.nlvls:
+            this.extend_nlvls(other.nlvls)
+        this._val = this._val ^ other.val()
+        return this
+
+    __sub__ = __add__
+
+    def __neg__(self):
+        return copy.copy(self)
+
+    def __mul__(self, other):
+        # support ndarray commutativity for scalar-vector product
+        if isinstance(other, np.ndarray):
+            return other * self
+        # align nlvls to max - a little suboptimal but all padded zeros are terminated by 0/1 optimizations
+        this = copy.copy(self)
+        if this.nlvls > other.nlvls:
+            other.extend_nlvls(this.nlvls)
+        if other.nlvls > this.nlvls:
+            this.extend_nlvls(other.nlvls)
+        # optimizations in case of 0 or 1
+        if self._val == 0 or other.val() == 1:
+            return this
+        if self._val == 1 or other.val() == 0:
+            return copy.copy(other)
+        # else
+        if max(this.nlvls, other.nlvls) > 1:
+            # 4b mult or higher
+            a_hi, a_lo = this.split()
+            b_hi, b_lo = other.split()
+            a_sum = a_hi + a_lo
+            b_sum = b_hi + b_lo
+
+            tmp = self._mul_abstract(a_hi, a_lo, a_sum, b_hi, b_lo, b_sum)
+        else:
+            # 2b mult
+            # mod: x_0^2 = x_0 + 1
+            # (a_hi * x_0 + a_lo)(b_hi * x_0 + b_lo) = (a_hi * b_hi + a_hi * b_lo + a_lo * b_hi) * x_0 +
+            #                                          (a_hi * b_hi + a_lo * b_lo)
+            a_hi = int(this.bin()[0], 2)
+            a_lo = int(this.bin()[1], 2)
+            b_hi = int(other.bin()[0], 2)
+            b_lo = int(other.bin()[1], 2)
+            a_sum = a_hi ^ a_lo
+            b_sum = b_hi ^ b_lo
+            lo = a_lo * b_lo
+            hi = a_sum * b_sum ^ lo
+            lo = a_hi * b_hi ^ lo
+            tmp = btf(2 * hi + lo, nlvls=1)
+        return tmp
+
+    @staticmethod
+    def _mul_abstract(a_hi, a_lo, a_sum, b_hi, b_lo, b_sum):
+        # mod: x_i^2 = x_i + (x_i >> 1)
+        # (a_hi * x_i + a_lo)(b_hi * x_i + b_lo) = (a_lo * b_lo) + ((a_hi * b_hi) * (x_i >> 1))
+        #                                          [ (a_hi * b_lo + a_lo * b_hi) + (a_hi * b_hi) ] * x_i +
+        # note that since x_i = v_{2^i}, the (2^i)'th basis element => (x_i >> 1) = v_{2^i-1}
+        lo = a_lo * b_lo
+        hi = a_sum * b_sum + lo
+        mx = a_hi * b_hi
+        mx = mx * btf(1 << (mx.nbits - 1), nlvls=mx.nlvls)
+        lo += mx
+        # concatenate
+        tmp = btf((hi.val() << hi.nbits) + lo.val(), nlvls=hi.nlvls + 1)
+        return tmp
+
+    def __pow__(self, power):
+        tmp = copy.copy(self)
+        # special case (not modulo)
+        if power == 0:
+            tmp.one()
+            return tmp
+        # standard cases
+        if tmp == 0 or tmp == 1:
+            return tmp
+        power %= (self._p - 1)
+        if power == 0:
+            tmp.one()
+            return tmp
+        elif power == 1:
+            return tmp
+        elif power == 2:
+            return tmp * tmp
+        else:
+            return tmp.__pow__(power % 2) * tmp.__pow__(power // 2) ** 2
+
+    def inv(self):
+        tmp = copy.copy(self)
+        return tmp ** (self._p - 2)
+
+
+if __name__ == '__main__':
+    nlvls = 2
+    for i in range(1, 2 ** (2 ** nlvls)):
+        a = btf(i, nlvls=nlvls)
+        for j in range(1, 2 ** (2 ** nlvls)):
+            b = btf(j, nlvls=nlvls)
+            c = b * a
+            print(f'{a} * {b} = {c}')
